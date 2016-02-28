5e707d3f36e32f0af8412dc0196a0a18beba7297
Max Paperno <max@wdg.us>
2016-02-28 18:26:49 -0500

    Add option/function for looking up the magnetic field inclination & declination automatically via online NOAA service (-automag). Move default starting reference point and mag field definitions to top.

--- src/me/drton/jmavsim/Simulator.java
+++ src/me/drton/jmavsim/Simulator.java
@@ -13,10 +13,13 @@ import javax.vecmath.Matrix3d;
 import javax.vecmath.Vector3d;
 import javax.xml.parsers.ParserConfigurationException;
 
-import java.lang.Math;
 import java.io.IOException;
+import java.io.InputStream;
+import java.lang.Math;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.Executors;
@@ -29,6 +32,7 @@ public class Simulator implements Runnable {
 
     public static boolean USE_SERIAL_PORT = false;
     public static boolean COMMUNICATE_WITH_QGC = true;
+    public static boolean DO_MAG_FIELD_LOOKUP = false;
     public static boolean SHOW_REPORT_PANEL = false;
     public static boolean GUI_START_MAXIMIZED = false;
     public static ViewTypes GUI_START_VIEW = ViewTypes.VIEW_FPV;
@@ -43,8 +47,23 @@ public class Simulator implements Runnable {
     public static final int    DEFAULT_SERIAL_BAUD_RATE = 230400;
     public static final String LOCAL_HOST = "127.0.0.1";
 
-    public static final int    DEFAULT_CAM_PITCH_CHAN =  4;    // Control gimbal pitch from autopilot, -1 to disable
-    public static final int    DEFAULT_CAM_ROLL_CHAN  = -1;    // Control gimbal roll from autopilot, -1 to disable
+    // Set global reference point
+    // Zurich Irchel Park: 47.397742, 8.545594, 488m
+    // Seattle downtown: 47.592182, -122.316031, 86m
+    // Moscow downtown: 55.753395, 37.625427, 155m
+    public static final LatLonAlt DEFAULT_ORIGIN_POS = new LatLonAlt(47.397742, 8.545594, 488);
+
+    // Mag vector in earth field. If Y value is left as zero, then the
+    //   declination will be added later based on the origin GPS position.
+    // If DO_MAG_FIELD_LOOKUP = true or -automag switch is used then this value is ignored.
+    // 
+    // Zurich:  (0.44831f, 0.01664f, 0.89372f)
+    // Seattle: (0.34252f, 0.09805f, 0.93438f)
+    // Moscow:  (0.31337f, 0.06030f, 0.94771f)
+    public static final Vector3d  DEFAULT_MAG_FIELD = new Vector3d(0.44831f, 0.01664f, 0.89372f);
+    
+    public static final int    DEFAULT_CAM_PITCH_CHAN =  4;     // Control gimbal pitch from autopilot, -1 to disable
+    public static final int    DEFAULT_CAM_ROLL_CHAN  = -1;     // Control gimbal roll from autopilot, -1 to disable
     public static final Double DEFAULT_CAM_PITCH_SCAL = 1.57;  // channel value to physical movement (+/-90 deg)
     public static final Double DEFAULT_CAM_ROLL_SCAL  = 1.57;  // channel value to physical movement (+/-90 deg)
 
@@ -78,11 +97,7 @@ public class Simulator implements Runnable {
     public Simulator() throws IOException, InterruptedException, ParserConfigurationException, SAXException {
         // Create world
         world = new World();
-        // Set global reference point
-        // Zurich Irchel Park: 47.397742, 8.545594, 488m
-        // Seattle downtown (15 deg declination): 47.592182, -122.316031, 86m
-        // Moscow downtown: 55.753395, 37.625427, 155m
-        LatLonAlt referencePos = new LatLonAlt(47.397742, 8.545594, 488.0);
+        LatLonAlt referencePos = DEFAULT_ORIGIN_POS;
         world.setGlobalReference(referencePos);
 
         MAVLinkSchema schema = new MAVLinkSchema("mavlink/message_definitions/common.xml");
@@ -129,24 +144,25 @@ public class Simulator implements Runnable {
         // Create environment
         SimpleEnvironment simpleEnvironment = new SimpleEnvironment(world);
 
-        // Mag vector in earth field, loosely based on the earth field in Zurich,
-        // but without declination. The declination will be added below based on
-        // the origin GPS position.
-        Vector3d magField = new Vector3d(0.21523, 0.0f, 0.42741);
-
         //simpleEnvironment.setWind(new Vector3d(0.0, 5.0, 0.0));
         //simpleEnvironment.setWindDeviation(0.0);
         simpleEnvironment.setGroundLevel(0.0f);
         world.addObject(simpleEnvironment);
 
-        // Set declination based on the initialization position of the Simulator
-        // getMagDeclination() returns degrees and variable decl is in radians.
-        double decl = Math.toRadians(world.getEnvironment().getMagDeclination(referencePos.lat, referencePos.lon));
-        //System.out.println("Declination: " + (Math.toDegrees(decl)));
-        
-        Matrix3d magDecl = new Matrix3d();
-        magDecl.rotZ(decl);
-        magDecl.transform(magField);
+        // Set up magnetic field deviations 
+        // (do this after environment already has a reference point in case we need to look up declination manually)
+        Vector3d magField = DEFAULT_MAG_FIELD;
+        if (DO_MAG_FIELD_LOOKUP)
+            magField = magFieldLookup(referencePos);
+        else if (magField.y == 0.0f) {
+            // Set declination based on the initialization position of the Simulator
+            // getMagDeclination() returns degrees and variable decl is in radians.
+            double decl = Math.toRadians(world.getEnvironment().getMagDeclination(referencePos.lat, referencePos.lon));
+            //System.out.println("Declination: " + (Math.toDegrees(decl)));
+            Matrix3d magDecl = new Matrix3d();
+            magDecl.rotZ(decl);
+            magDecl.transform(magField);
+        }
         simpleEnvironment.setMagField(magField);
 
         // Create vehicle with sensors
@@ -270,11 +286,66 @@ public class Simulator implements Runnable {
             executor.shutdown();
         }
     }
+
+    /**
+     * Look up the magnetic inclination and declination for a given Lat/Lon/Alt using a NOAA Web service.
+     * If successful, returns a valid Vector3d() suitable for setting the magnetic field in the simulated environment.
+     * Also displays the resulting vector so it can be copied to the DEFAULT_MAG_FIELD setting to avoid future lookups.
+     *
+     * @param pos {@link me.drton.jmavlib.geo.LatLonAlt} object of reference point.
+     * @return Vector3d The magnetic field variance vector, or Vector3d(0,0,0) if lookup failed.
+     */
+    public static Vector3d magFieldLookup(LatLonAlt pos) {
+
+        Double decl;
+        Double incl;
+        Vector3d magField = new Vector3d(0.0f, 0.0f, 0.0f);
+        String resp, vals[];
+        
+        String reqUrl = "http://www.ngdc.noaa.gov/geomag-web/calculators/calculateIgrfwmm?";
+        reqUrl += "resultFormat=csv&coordinateSystem=M&lat1=" + pos.lat + "&lon1=" + pos.lon + "&elevation=" + pos.alt / 1e3;
+        System.out.println("Attempting magnetic field data lookup from NOAA...");
+        try {
+            InputStream instr = new URL(reqUrl).openStream();
+            Scanner scan = new Scanner(instr, "UTF-8");
+            resp = scan.useDelimiter("\\A").hasNext() ? scan.next() : "";
+            String lines[] = resp.split("\n");
+            vals = lines[lines.length-1].split(",");
+            scan.close();
+            instr.close();
+        } catch (IOException e) {
+            System.err.println("Error requesting URL: " + reqUrl + "\n");
+            return magField;
+        }
+
+        if (vals.length > 3) {
+            try {
+                decl = new Double(vals[1]);
+                incl = new Double(vals[2]);
+            } catch (NumberFormatException e) {
+                System.err.println("Error parsing response: " + resp + "\n");
+                return magField;
+            }
+            System.out.println("Lookup Declination: " + decl + "; Inclination: " + incl);
+            decl = Math.toRadians(decl);
+            incl = Math.toRadians(incl);
+            magField = new Vector3d(Math.cos(incl), 0.0f, Math.sin(incl));
+            Matrix3d declMtx = new Matrix3d();
+            declMtx.rotZ(decl);
+            declMtx.transform(magField);
+            System.out.printf("Result Vectors: Vector3d(%.5f, %.5f, %.5f) \n", magField.x, magField.y, magField.z);
+            System.out.printf("       Declination: %.5f; Inclination: %.5f \n", Math.toDegrees(Math.atan2(magField.y, magField.x)), Math.toDegrees(Math.atan2(magField.z, magField.x)));
+        } else
+            System.err.println("Error parsing response: " + resp + "\n");
+        
+        return magField;
+    }
     
     public final static String PRINT_INDICATION_STRING = "-m [<MsgID[, MsgID]...>]";
     public final static String UDP_STRING = "-udp <mav ip>:<mav port>";
     public final static String QGC_STRING = "-qgc"; // <qgc ip address>:<qgc peer port> <qgc bind port>
     public final static String SERIAL_STRING = "-serial [<path> <baudRate>]";
+    public final static String MAG_STRING = "-automag";
     public final static String REP_STRING = "-rep";
     public final static String GUI_MAX_STRING = "-max";
     public final static String GUI_VIEW_STRING = "-view (fpv|grnd|gmbl)";
@@ -282,7 +353,7 @@ public class Simulator implements Runnable {
     public final static String SPEED_STRING = "-r <Hz>";
     public final static String CMD_STRING = "java -cp lib/*:out/production/jmavsim.jar me.drton.jmavsim.Simulator";
     public final static String CMD_STRING_JAR = "java -jar jmavsim_run.jar";
-    public final static String USAGE_STRING = CMD_STRING_JAR + " [-h] [" + UDP_STRING + " | " + SERIAL_STRING + "] [" + SPEED_STRING + "] [" + AP_STRING + "] " + 
+    public final static String USAGE_STRING = CMD_STRING_JAR + " [-h] [" + UDP_STRING + " | " + SERIAL_STRING + "] [" + SPEED_STRING + "] [" + AP_STRING + "] [" + MAG_STRING + "] " + 
                                               "[" + QGC_STRING + "] [" + GUI_MAX_STRING + "] [" + GUI_VIEW_STRING + "] [" + REP_STRING + "] [" + PRINT_INDICATION_STRING + "]";
 
     public static void main(String[] args)
@@ -450,6 +521,8 @@ public class Simulator implements Runnable {
                     System.err.println("-view requires an argument: " + GUI_VIEW_STRING);
                     return;
                 }
+            } else if (arg.equals("-automag")) {
+                DO_MAG_FIELD_LOOKUP = true;
             } else if (arg.equals("-rep")) {
                 SHOW_REPORT_PANEL = true;
             } else if (arg.equals("-max")) {
@@ -482,6 +555,8 @@ public class Simulator implements Runnable {
         System.out.println("      Default is " + DEFAULT_SIM_SPEED + " Hz\n");
         System.out.println(AP_STRING);
         System.out.println("      Specify the MAV type. E.g. 'px4' or 'aq'. Default is: " + autopilotType + "\n");
+        System.out.println(MAG_STRING);
+        System.out.println("      Attempt automatic magnetic field inclination/declination lookup for starting position via NOAA Web service.\n");
         System.out.println(QGC_STRING);
         System.out.println("      Forward message packets to QGC via UDP at " + qgcIpAddress + ":" + qgcPeerPort + " bind:" + qgcBindPort + "\n");
         System.out.println(GUI_VIEW_STRING);
