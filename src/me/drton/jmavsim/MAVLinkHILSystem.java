eca8ecc447f41a9165a8f7b1f7f3cd020dd15bb1
Lorenz Meier <lorenz@px4.io>
2017-03-11 21:36:19 +0100

    Fix Java version incompatibility for Vector3D

--- src/me/drton/jmavsim/MAVLinkHILSystem.java
+++ src/me/drton/jmavsim/MAVLinkHILSystem.java
@@ -186,19 +186,19 @@ public class MAVLinkHILSystem extends MAVLinkSystem {
             msg_hil_state.set("attitude_quaternion", q);
 
             Vector3d v3d = vehicle.getRotationRate();
-            msg_hil_state.set("rollspeed", (float) v3d.getX());
-            msg_hil_state.set("pitchspeed", (float) v3d.getY());
-            msg_hil_state.set("yawspeed", (float) v3d.getZ());
+            msg_hil_state.set("rollspeed", (float) v3d.x);
+            msg_hil_state.set("pitchspeed", (float) v3d.y);
+            msg_hil_state.set("yawspeed", (float) v3d.z);
 
-            int alt = (int) (1000 * vehicle.position.getZ());
+            int alt = (int) (1000 * vehicle.position.z);
             msg_hil_state.set("alt", alt);
             msg_hil_state.set("lat", (int)(sensors.getGlobalPosition().lat * 1.e7));
             msg_hil_state.set("lon", (int)(sensors.getGlobalPosition().lon * 1.e7));
 
             v3d = vehicle.getVelocity();
-            msg_hil_state.set("vx", (int) (v3d.getX() * 100));
-            msg_hil_state.set("vy", (int) (v3d.getY() * 100));
-            msg_hil_state.set("vz", (int) (v3d.getZ() * 100));
+            msg_hil_state.set("vx", (int) (v3d.x * 100));
+            msg_hil_state.set("vy", (int) (v3d.y * 100));
+            msg_hil_state.set("vz", (int) (v3d.z * 100));
 
             Vector3d airSpeed = new Vector3d(vehicle.getVelocity());
             airSpeed.scale(-1.0);
@@ -207,9 +207,9 @@ public class MAVLinkHILSystem extends MAVLinkSystem {
             msg_hil_state.set("true_airspeed", (int) (as_mag * 100));
 
             v3d = vehicle.acceleration;
-            msg_hil_state.set("xacc", (int) (v3d.getX() * 1000));
-            msg_hil_state.set("yacc", (int) (v3d.getY() * 1000));
-            msg_hil_state.set("zacc", (int) (v3d.getZ() * 1000));
+            msg_hil_state.set("xacc", (int) (v3d.x * 1000));
+            msg_hil_state.set("yacc", (int) (v3d.y * 1000));
+            msg_hil_state.set("zacc", (int) (v3d.z * 1000));
 
             sendMessage(msg_hil_state);
             nextHilStatePub = tu + hilStateUpdateInterval;
